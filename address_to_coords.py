58e8961550738150818d52ebee71eec97e4d2d1b
Jason Fehr <jasonmfehr@gmail.com>
2025-10-23 22:08:22 -0700

    added script to update db rows with latitude/longitude values
    added script to do a quick search of the Nominatim database
    
    added file of addresses not found

--- address_to_coords.py
+++ address_to_coords.py
@@ -15,8 +15,6 @@ with duckdb.connect(database=sys.argv[1], read_only=False) as conn:
     results = conn.execute('select "Geo ID","Sale Date", Address from sales_data WHERE city = \'PORT ANGELES\' order by "Geo ID"').fetchall()
 
     for row in results:
-        print(row)
-        import pdb;pdb.set_trace()
         address = f"{row[2]}, Clallam County, WA, USA"
         result = nominatim.query(address)
 
@@ -24,9 +22,9 @@ with duckdb.connect(database=sys.argv[1], read_only=False) as conn:
         if result and result.toJSON():
             lat = result.toJSON()[0]['lat']
             lon = result.toJSON()[0]['lon']
-            print(f"{address} => Latitude: {lat}, Longitude: {lon}")
+            conn.execute(f'update sales_data set lat={lat}, lng={lon} where "geo id" = \'{row[0]}\' and "sale date" = \'{row[1]}\'')
         else:
-            print("Address not found")
+            print(f"Address not found: {address}")
 
 # address = "174, Camelot Road, Clallam County, WA, USA"
 # # address = "1600 Amphitheatre Parkway, Mountain View, CA, USA"
